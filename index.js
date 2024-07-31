4ef777950efcb773511817880bba612bab2ead58
Thales Gonçalves <59776760+thales-goncalves3@users.noreply.github.com>
2024-07-31 15:48:28 -0300

    enviar cep pela url

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const axios = require('axios');
 
 const app = express();
 
+
+app.use(express.urlencoded({ extended: true })); // Para analisar dados URL-encoded
 app.use(express.json()); 
 
 async function getCepData(cep) {
   try {
     const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-    console.log(response.data);
+   
     return response.data;
   } catch (error) {
     throw new Error('Erro ao buscar o CEP: ' + error.message);
@@ -20,19 +22,21 @@ app.get('/', (request, response) => {
   return response.json({ message: 'tsdasdi' });
 });
 
-app.post('/cepdosguri/', async (request, response) => {
-  const { cep } = request.body;
-
-  if (!cep) {
-    return response.status(400).json({ error: 'CEP não fornecido' });
-  }
-
-  try {
-    const data = await getCepData(cep);
-    return response.json(data);
-  } catch (error) {
-    return response.status(500).json({ error: error.message });
-  }
+app.post('/cepdosguri/:cep', async (request, response) => {
+    
+  
+    const cep = request.params.cep;
+  
+    if (!cep) {
+      return response.status(400).json({ error: 'CEP não fornecido' });
+    }
+  
+    try {
+      const data = await getCepData(cep);
+      return response.json(data);
+    } catch (error) {
+      return response.status(500).json({ error: error.message });
+    }
 });
 
 app.listen(3333, () => {
